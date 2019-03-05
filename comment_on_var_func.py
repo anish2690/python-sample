c65aed8f94437e0a66ac5317d81d6c7cdb02dc38
Anish <anishgeorge2690@gmail.com>
2019-03-05 15:17:28 +0530

    add: comment inside array and function

---
+++ comment_on_var_func.py
@@ -0,0 +1,22 @@
+a = [1, 2, 3, 5]
+print(a)
+
+b = [1  # jhfbdfjg
+     ,
+     2, 3, 5]
+print(b)
+
+c = (1, 2, 3, 54)
+
+print(c)
+
+
+def my_func(a,  # value of a
+            b,  # value of b
+            ):
+    print(a, b)
+
+
+my_func(10,  # a value
+        20  # b value
+        )
